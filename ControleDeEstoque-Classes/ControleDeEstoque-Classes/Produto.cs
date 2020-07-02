d9adaebc415a9323671bb1ffc6be5adf515d54bd
Luiz Rech <luizadre@hotmail.com>
2020-07-02 14:16:56 -0300

    Exercicio para explicar classes

---
+++ ControleDeEstoque-Classes/ControleDeEstoque-Classes/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControleDeEstoque_Classes
+{
+    class Produto
+    {
+        public string Nome;
+        public int Quantidade;
+        public double Preco;
+
+        public double TotalEmEstoque(){
+            return Quantidade * Preco;
+        }
+
+        public void AddProduto(int quantidade){
+            Quantidade += quantidade;
+        }
+        public void DelProduto(int quantidade){
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, valor total em estoque: $ "
+                + TotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ControleDeEstoque-Classes/ControleDeEstoque-Classes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControleDeEstoque_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            
+            Console.WriteLine("Dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+            Console.Write("Preco: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(p);
+
+            Console.WriteLine("Adicionar qtd no estoque: ");
+            int addqtd = int.Parse(Console.ReadLine());
+            p.AddProduto(addqtd);
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine("Remover qtd no estoque: ");
+            int delqtd = int.Parse(Console.ReadLine());
+            p.DelProduto(delqtd);
+
+            Console.WriteLine("Dados atualizados: " + p);
+        
+        }
+    }
+}
