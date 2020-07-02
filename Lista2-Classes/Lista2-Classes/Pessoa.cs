9b82accf68a1b01a758a748e89afbf225aa7ad6f
Luiz Rech <luizadre@hotmail.com>
2020-07-02 14:15:10 -0300

    Lista 2 - CLasses

---
+++ Lista2-Classes/Lista2-Classes/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lista2_Classes
+{
+    class Pessoa
+    {
+        public string name;
+        public int age;
+    }
+}
