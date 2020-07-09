a59eb4731fb8d360a4e0d34a9bddc28cd4f6d4b2
Luiz Rech <luizadre@hotmail.com>
2020-07-09 17:28:12 -0300

    Sistema de Hotel - Exercicio Proposto vetores

---
+++ SistemaHotel-ExercicioVetores/SistemaHotel-ExercicioVetores/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaHotel_ExercicioVetores
+{
+    class Cliente
+    {
+        public string Nome;
+        public string Email;
+
+        public override string ToString()
+        {
+            return Nome
+                + ", "
+                + Email;
+                
+        }
+    }
+}
---
+++ SistemaHotel-ExercicioVetores/SistemaHotel-ExercicioVetores/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace SistemaHotel_ExercicioVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos vao ser alugados?");
+            int quartosAlugados = int.Parse(Console.ReadLine());
+
+            Cliente[] cl = new Cliente[10];
+
+            for (int i = 0; i < quartosAlugados; i++)
+            {
+                Console.WriteLine("Cliente #"+ (i + 1) +":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int nroQuarto = int.Parse(Console.ReadLine());
+
+                cl[nroQuarto] = new Cliente { Email = email, Nome = nome };
+            }
+
+            for (int i = 0; i < 10; i++){
+                if(cl[i] != null)
+                {
+                    Console.WriteLine( i + ": " + cl[i]);
+                }
+            }
+        }
+    }
+}
