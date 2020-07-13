1201b415b7aa78a698c4447fec57158bfbe5d058
Luiz Rech <luizadre@hotmail.com>
2020-07-13 15:37:12 -0300

    Exercicio de Matrizes 1

---
+++ Matrizes/Exercicio1-Matrizes/Exercicio1-Matrizes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exercicio1_Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeMat = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[sizeMat, sizeMat];
+
+            Console.WriteLine("Digite os valores: ");
+
+            for (int i = 0; i < sizeMat; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for(int j = 0; j < sizeMat; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            for(int i = 0; i < sizeMat; i++)
+            {
+                Console.WriteLine();
+                for(int j = 0; j < sizeMat; j++)
+                {
+                    Console.Write(mat[i, j] + " ");
+                }
+            }
+
+            Console.Write("Main Diagonal: ");
+            for (int i = 0; i < sizeMat; i++){
+                for(int j = 0; j < sizeMat; j++)
+                {
+                    if(i == j)
+                    {
+                        Console.Write(mat[i, j] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
